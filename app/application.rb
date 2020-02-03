92b7313a8ac284b1f5b8c57f12c30532b302d35b
Josh Morris <jmo0624@gmail.com>
2020-02-03 23:40:13 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,5 +9,5 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       
-      item = @@items.find{|i| i.name 
+      item = @@items.find{|i| i.name == item_name}
       
